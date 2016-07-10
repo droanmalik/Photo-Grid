99e65b4b3a96fe0a539c952494a17c3222f11832
Droan Malik <droanmalik@gmail.com>
2016-07-11 00:25:23 +0530

    Initial commit

---
+++ app/src/main/java/me/droan/photo_grid/photos/PhotosActivity.java
@@ -0,0 +1,15 @@
+package me.droan.photo_grid.photos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.droan.photo_grid.R;
+
+public class PhotosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/me/droan/photo_grid/photos/PhotosPresenter.java
@@ -0,0 +1,7 @@
+package me.droan.photo_grid.photos;
+
+/**
+ * Created by drone on 11-07-2016.
+ */
+public class PhotosPresenter {
+}
